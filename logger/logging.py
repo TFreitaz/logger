f852f78804927104220c169284e8a6f47f35499b
Thales Freitas <thales.zfreitas@gmail.com>
2021-02-09 18:48:50 -0300

    Add dict parameter

--- logger/logging.py
+++ logger/logging.py
@@ -70,7 +70,7 @@ class Logger:
         self.error_name = None
         self.exception_message = None
 
-        self.dict = {}
+        self.dict: dict = {}
 
         if not env_path:
             env_path = find_dotenv()
